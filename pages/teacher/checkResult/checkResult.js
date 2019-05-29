68e1487f3a7556c75f3022163f047a58ff6babb5
allenadom <enthusiast521@outlook.com>
2019-05-29 16:59:39 +0800

    完成了补签功能

--- pages/teacher/checkResult/checkResult.js
+++ pages/teacher/checkResult/checkResult.js
@@ -1,8 +1,7 @@
 
-
 Page({
   data: {
-   
+    isDisabled:false
     
   },
   onLoad: function (options) {
@@ -38,33 +37,46 @@ Page({
     var that = this;
     var sId = event.currentTarget.dataset.sid;
     var tId = this.data.tId;
-    wx.request({
-      url: 'http://localhost:8080/crs/teacher/updateResign.shtml',
-      //定义传到后台的数据
-      data: {
-        tId: tId,
-        sId: sId,
-      },
-      method: 'post',//定义传到后台接受的是post方法还是get方法
-      header: {
-        'content-type': 'application/x-www-form-urlencoded' // 默认值
-      },
-      success: function (res) {
-        wx.showToast({
-          title: '补签成功！',
-          icon: 'succes',
-          duration: 1000,
-          mask: true
-        });  
-        var num = ++that.data.currentCnt;
-        that.setData({
-          currentCnt: num
-        });
-      },
-      fail: function (res) {
-        console.log("调用API失败");
-      }
-    });
+      wx.request({
+        url: 'http://localhost:8080/crs/teacher/updateResign.shtml',
+        //定义传到后台的数据
+        data: {
+          tId: tId,
+          sId: sId,
+        },
+        method: 'post',//定义传到后台接受的是post方法还是get方法
+        header: {
+          'content-type': 'application/x-www-form-urlencoded' // 默认值
+        },
+        success: function (res) {
+          var result = res.data.result;
+          if(result == true){
+            wx.showToast({
+              title: '补签成功！',
+              icon: 'success',
+              duration: 1000,
+              mask: true
+            });
+            var num = ++that.data.currentCnt;
+            that.setData({
+              currentCnt: num
+            });
+          }
+          else{
+            wx.showToast({
+              title: '不可重复补签！',
+              icon: 'loading',
+              duration: 1000,
+              mask: true
+            });
+          }
+        },
+        fail: function (res) {
+          console.log("调用API失败");
+        }
+      });
+ 
+    
     
   },
 summitla:function(){
--- pages/teacher/identifyCode/identifyCode.js
+++ pages/teacher/identifyCode/identifyCode.js
@@ -37,13 +37,33 @@ Page({
         that.setData({
           cnt: res.data.cnt
         });
+        wx.request({
+          url: 'http://localhost:8080/crs/teacher/getcurrentCnt.shtml',
+          //定义传到后台的数据
+          data: {
+            tId: that.data.tId
+          },
+          method: 'post',//定义传到后台接受的是post方法还是get方法
+          header: {
+            'content-type': 'application/x-www-form-urlencoded' // 默认值
+          },
+          success: function (res) {
+            that.setData({
+              currentCnt: res.data.currentCnt
+            });
+
+          },
+          fail: function (res) {
+            console.log("调用API失败");
+          }
+        });
 
         interval = setInterval(function () {
           wx.request({
             url: 'http://localhost:8080/crs/teacher/getcurrentCnt.shtml',
           //定义传到后台的数据
           data: {
-            tId: '2010001'         
+            tId: that.data.tId         
           },
           method: 'post',//定义传到后台接受的是post方法还是get方法
           header: {
